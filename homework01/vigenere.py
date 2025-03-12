54942ddc581e312cde949a6b3aca74573703138d
Тимур <khazratkulov8@gmail.com>
2025-03-12 11:38:53 +0300

    Реализованы функции encrypt_vigenere и decrypt_vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,4 +1,6 @@
-def encrypt_vigenere(plaintext, keyword):
+import caesar
+
+def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
 
@@ -9,11 +11,22 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+    ciphertext = ''
+    index = 0
+    for i in plaintext:
+        if (index >= len(keyword)):
+            index = 0   
+        if ("a" <= i <= "z"):
+            ciphertext += caesar.encrypt_caesar(i, ord(keyword[index].lower()) - ord("a"))
+        elif ("A" <= i <= "Z"):
+            ciphertext += caesar.encrypt_caesar(i, ord(keyword[index].upper()) - ord("A"))
+        else:
+            ciphertext += i
+        index += 1
     return ciphertext
 
 
-def decrypt_vigenere(ciphertext, keyword):
+def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
 
@@ -24,5 +37,16 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    # PUT YOUR CODE HERE
+    plaintext = ""
+    index = 0
+    for i in ciphertext:
+        if (index >= len(keyword)):
+            index = 0   
+        if ("a" <= i <= "z"):
+            plaintext += caesar.decrypt_caesar(i, ord(keyword[index].lower()) - ord("a"))
+        elif ("A" <= i <= "Z"):
+            plaintext += caesar.decrypt_caesar(i, ord(keyword[index].upper()) - ord("A"))
+        else:
+            plaintext += i
+        index += 1
     return plaintext
